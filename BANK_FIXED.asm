aa469d1468f621c0f00d062a52229ce529e065fb
Andrew Davie <boo@Andrews-MacBook-Pro.local>
2019-07-28 12:48:11 +1000

    first "clean" version without IP issues

--- BANK_FIXED.asm
+++ BANK_FIXED.asm
@@ -1054,11 +1054,31 @@ rbret           lda ROM_Bank
     ;---------------------------------------------------------------------------
 
     DEFINE_SUBROUTINE nextLevelMan
- jmp nextLevelMan
 
-                lda #BANK_NextLevelX
-                sta SET_BANK
-                jmp NextLevelX
+                ;lda #BANK_NextLevelX
+                ;sta SET_BANK
+                ;jmp NextLevelX
+
+   ; Now do the actual switching
+
+               lda NextLevelTrigger
+               and #<(~BIT_NEXTLEVEL)
+               sta NextLevelTrigger
+
+   ; Next level is due. Point to the next level, but if we're at the end of playable levels,
+   ; then increment the level number. This is completely circular, so we eventually wrap
+   ; the level back to 0 and start afresh.
+
+               lda levelX
+               clc
+               adc #LEVEL_DEFINITION_SIZE
+               cmp #LEVELNUM
+               bcc .level_ok
+
+               lda #0
+.level_ok       sta levelX
+               rts
+
 
 
     ;---------------------------------------------------------------------------
